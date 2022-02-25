66ad0c05ddda1df2105bb895954dbd9f51d5e081
Naveen Kumar Jain <you@example.com>
2022-02-25 16:34:11 +0530

    added new features to ecoomerce db

--- ecommerce-database-design.sql
+++ ecommerce-database-design.sql
@@ -95,3 +95,96 @@ values('Dell Vostro' , 10 , 'Innovaccer Analytics Pvt. Ltd.' , now());
 update inventory set qty = qty + 100 where name = 'Dell Vostro';
 
 
+/* create a log file for all sales , purchases */
+
+create table audit_trails(
+	id SERIAL PRIMARY KEY,
+	type VARCHAR(50),
+	client VARCHAR(50),
+	order_date DATE,
+	pid INT,
+	qty INT,
+	amount INT
+)
+
+/* created a purchase memo */
+create table purchases(
+	id SERIAL PRIMARY KEY,
+	product_name VARCHAR(50),
+	distributor VARCHAR(50),
+	qty INT,
+	rate INT
+)
+
+/* added new column for tracking daily purchases */
+alter table report add purchases INT;
+update report set purchases = 0;
+
+/* function for adding log for every sale */
+
+CREATE OR REPLACE FUNCTION fn_audit_trail()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+as 
+$$
+BEGIN
+    IF NEW.order_qty <= (select qty from inventory where name = NEW.product_name) THEN
+         insert into audit_trails(type, client, order_date, pid, qty, amount) values 
+		 ('SALE', NEW.customer_name, NOW(), (select pid from inventory where name = NEW.product_name),
+		   NEW.order_qty , NEW.order_qty * (select MRP from inventory where name = NEW.product_name));
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trigger_build_audit_trail
+BEFORE INSERT
+ON manage_orders
+FOR EACH ROW 
+EXECUTE PROCEDURE fn_audit_trail();
+
+insert into manage_orders (product_name , order_qty , customer_name , order_date)
+values('HP Pavilion' , 2 , 'Kartik SIngh' , now());
+
+
+/* function for adding log at every purchase  , updating the inventory , updating the daily purchases */
+
+CREATE OR REPLACE FUNCTION fn_purchase()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+as
+$$
+BEGIN
+	update inventory set qty = qty+NEW.qty where name = NEW.product_name;
+	insert into audit_trails(type, client, order_date, pid, qty, amount) values 
+		 ('PURCHASE', NEW.distributor, NOW(), (select pid from inventory where name = NEW.product_name),
+		   NEW.qty , NEW.qty*NEW.rate);
+	update report set purchases = purchases + NEW.qty * NEW.rate;
+	RETURN NEW;
+END;
+$$;
+
+
+
+CREATE TRIGGER trigger_on_purchase
+BEFORE INSERT
+ON purchases
+FOR EACH ROW
+EXECUTE PROCEDURE fn_purchase();
+
+/* event 'purchase' happened */
+insert into purchases (product_name , distributor , qty , rate) 
+values('MacBook Pro' , 'Mango Distributors' , 100 , 125000);
+
+
+
+
+
+
+
+
+
+
+
+
+
